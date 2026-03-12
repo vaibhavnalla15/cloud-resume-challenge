882db1264bb1916943de7a5b088e0b12dfe12a18
vaibhav <vaibhavnalla15@gmail.com>
2026-03-12 18:04:19 +0530

    Added frontend for cloud resume challenge

---
+++ frontend/js/resume.js
@@ -0,0 +1,36 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#sideNav'
+  });
+
+})(jQuery); // End of use strict
+
+
+// Fetch visitor counter:- (with API Gateway URL)
+fetch("YOUR_API_GATEWAY_URL")
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById("visitor-count").innerText = data.count;
+  });
